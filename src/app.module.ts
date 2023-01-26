9888b80e577d35ce2b0e6d7fd28e34e063f28e4c
Vinicius Nogueira <viniciusepn@gmail.com>
2023-01-26 15:46:07 -0300

    configuração mongoDb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:GoaTpTioz60TFRUl@cluster0.tprzocn.mongodb.net/samrtranking?retryWrites=true&w=majority',
+      {
+        useNewUrlParse: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    ),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
